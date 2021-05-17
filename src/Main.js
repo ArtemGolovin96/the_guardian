666eb5300497c04284b405384ff0bab47fc0bd89
Artem Golovin <artempotapkin96@icloud.com>
2021-05-17 18:04:36 +0300

    pagination

--- src/Main.js
+++ src/Main.js
@@ -3,6 +3,8 @@ import React, { Component, useEffect, useState } from 'react';
 export default function Main () {
 
     const [arrNews, setNews] = useState([]);
+    const [page, setPage] = useState(0);
+    const [query1, setQuery] = useState(0)
 
 
 
@@ -11,26 +13,36 @@ export default function Main () {
         const formData = new FormData(e.target);
         const data = [...formData.values()];
         let query = data[0];
-        getFatchData(query).then((data) => {
+        setQuery(query);
+        getFatchData(query, 1   ).then((data) => {
             setNews(data.response.results);
         })
     }
 
-    const getFatchData = async (arg) => {
-        return await fetch(`https://content.guardianapis.com/search?q=${arg}&api-key=3ae4a796-1d49-4d04-8974-e3962e2fb188`)
+    const getFatchData = async (arg, page) => {
+        return await fetch(`https://content.guardianapis.com/search?page=${page}&q=${encodeURIComponent(arg)}&api-key=3ae4a796-1d49-4d04-8974-e3962e2fb188`)
         .then((res) => {
             return res.json();
+        })
+    }
+
+
+    function onClickPage(e) {
+        getFatchData(query1, e.target.textContent).then((data) => {
+            setNews(data.response.results);
         })
     }
 
  
 
     useEffect(() => {
-        getFatchData().then((data) => {
+        getFatchData('', 1).then((data) => {
             setNews(data.response.results);
         })
         
     }, [])
+
+
 
 
         return (
@@ -44,12 +56,19 @@ export default function Main () {
                             (arrNews && arrNews.length > 0)
                             ?
                                 arrNews.map((el, index) => {
-                                    return <ol><a className="pnews" href={el.webUrl}> {index+1})  {el.webTitle}</a></ol>
+                                    return <ol><a className="pnews"  target="_blank" href={el.webUrl}> {index+1})  {el.webTitle}</a></ol>
                                         })
                             : 
                                 <p className="errdata">Введите корректные данные</p>
                         }
+                            <section className="buttonpagecontainer">
+                                <p className="pagep">Страницы:</p>
+                                <button className="pagebutton" onClick={onClickPage}>1</button>
+                                <button className="pagebutton" onClick={onClickPage}>2</button>
+                                <button className="pagebutton" onClick={onClickPage}>3</button>
+                            </section>
                     </section>
+
             </main>
         )
 }
